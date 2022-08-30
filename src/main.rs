b5696605770da93b2d42c3306725fd118034ead8
jervw <vuolajere@gmail.com>
2022-08-30 18:28:56 +0300

    start from scratch

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,7 @@
-use yew::prelude::*;
-use yew_router::prelude::*;
+mod app;
 
-mod pages;
-use pages::{home::Home, page_not_found::PageNotFound, start::Start};
+use app::App;
 
-#[derive(Routable, PartialEq, Clone, Debug)]
-pub enum Route {
-    #[at("/")]
-    Home,
-    #[not_found]
-    #[at("/404")]
-    NotFound,
-    #[at("/start")]
-    Start,
-}
-
-pub enum Msg {
-    ToggleNavbar,
-}
-
-pub struct App {
-    navbar_active: bool,
-}
-impl Component for App {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            navbar_active: false,
-        }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::ToggleNavbar => {
-                self.navbar_active = !self.navbar_active;
-                true
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            <BrowserRouter>
-                <main>
-                    <Switch<Route> render={Switch::render(switch)} />
-                </main>
-                <footer class="footer">
-                    <p>{ "Powered by Yew" }</p>
-                </footer>
-            </BrowserRouter>
-        }
-    }
-}
-
-fn switch(routes: &Route) -> Html {
-    match routes.clone() {
-        Route::Home => {
-            html! { <Home /> }
-        }
-        Route::NotFound => {
-            html! { <PageNotFound /> }
-        }
-        Route::Start => {
-            html! { <Start /> }
-        }
-    }
-}
-
-// entry
 fn main() {
     yew::start_app::<App>();
 }
--- src/pages/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod home;
-pub mod page_not_found;
-pub mod start;
