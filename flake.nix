72e8ea323ecaeca0d6903e700234bd5b6bfe4530
jervw <vuolajere@gmail.com>
2023-08-08 16:43:46 +0300

    add nix flakes, remove CNAME

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    rust-overlay = { url = "github:oxalica/rust-overlay"; };
+  };
+
+  outputs = { nixpkgs, rust-overlay, ... }:
+    let system = "x86_64-linux";
+    in {
+      devShell.${system} = let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlay ];
+        };
+      in (({ pkgs, ... }:
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-analyzer
+            cargo
+            nodePackages.sass
+            trunk
+            wasm-bindgen-cli
+            wasm-pack
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            })
+          ];
+
+          shellHook = "";
+        }) { pkgs = pkgs; });
+    };
+}
